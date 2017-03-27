e4978c8fe9202b75c1d38d8c0897b97bb90d76d6
themartinsjoseph <themartinsjoseph@gmail.com>
2017-03-27 15:02:59 -0700

    maybe?

--- controllers/twilioClient.js
+++ controllers/twilioClient.js
@@ -1,20 +1,28 @@
 require("dotenv").config();
+var express = require('express');
+var router = express.Router(); 
 var accountSid = process.env.TWILIO_ACCOUNT_SID;
 var authToken = process.env.TWILIO_AUTH_TOKEN;
 
 var client = require('twilio')(accountSid, authToken);
 
-client.messages.create({
-  to: '+12063840852',
-  from: process.env.TWILIO_NUMBER,
-  body: 'whatever the fuck'
-}, function(err, message) {
-  if(err) {
-    console.log(err);
-  } else {
-    console.log(message.body);
-  }
-});
+sendMsg = function(to, message){
+  client.messages.create({
+    to: to,
+    from: process.env.TWILIO_NUMBER,
+    body: message
+  }, function(err, message) {
+    if(err) {
+      console.log(err);
+    } else {
+      console.log(message.body);
+    }
+  });
+}
+
+
+//Export 
+module.exports = router; 
 
 
 
