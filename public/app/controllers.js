e644ad757ca740a8966b723f98598081152f89d2
themartinsjoseph <themartinsjoseph@gmail.com>
2017-04-02 17:32:51 -0700

    updated Admin() in NavCtrl and AdminCtrl

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -151,7 +151,7 @@ angular.module('AppCtrl', ['AppServices'])
     });
     $scope.Admin = function(){
         console.log($scope.number + " VS 14252238606")
-        if ($scope.number == ('14252238606' || "12063840852")){
+        if (($scope.number == '14252238606') || ($scope.number == '12063840852')){
             return true;
         } else {
             return false;
@@ -347,7 +347,7 @@ angular.module('AppCtrl', ['AppServices'])
     UsersAPI.getUser(curUser).then(function(user){
         $scope.number = user.data.number
         $scope.Admin = function(){
-            if ($scope.number == (+14252238606 || +12063840852)){
+            if (($scope.number == +14252238606) || ($scope.number ==  +12063840852)){
                 return true;
             } else {
                 return false;
